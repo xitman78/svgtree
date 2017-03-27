1e4aaaae758a043e94743a5c197962d5657bb12c
Alexander Cherepnya <xitman78@me.com>
2017-03-27 23:19:28 +0300

    Minor fix for scaling

--- src/helpers/artBoardConsts.js
+++ src/helpers/artBoardConsts.js
@@ -1,10 +1,6 @@
-export const minScale = 0.25;
-export const maxScale = 2.0;
-
 export const scaleSteps = [
   0.25, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0
 ];
 
-// export default {
-//
-// };
+export const minScale = scaleSteps[0];
+export const maxScale = scaleSteps[scaleSteps.length - 1];
